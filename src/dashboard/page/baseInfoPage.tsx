84a84f8a7f7f9a938e560a82ff0e78f894ba50c3
shikai liu <lsk569937453@gmail.com>
2024-10-14 10:39:01 +0800

    Update the config of language.

--- src/dashboard/page/baseInfoPage.tsx
+++ src/dashboard/page/baseInfoPage.tsx
@@ -42,7 +42,7 @@ export function BaseInfoPage() {
             <Card className="pt-4">
                 <CardContent className="flex flex-col gap-5 text-right">
                     <div className="flex flex-row gap-10">
-                        <p className="basis-3/12 text-lg font-bold">{t('generalPage.projectName')}</p>
+                        <p className="basis-3/12 text-lg font-bold">{t('generalPage.projectName')}:</p>
                         <p className="text-lg">{baseInfo?.project_name}</p>
                     </div>
                     <div className="flex flex-row gap-10 text-right">
@@ -65,12 +65,21 @@ export function BaseInfoPage() {
                     </div>
                     <div className="flex flex-row gap-10 text-right">
                         <p className="basis-3/12 text-lg font-bold">{t('generalPage.totalLines')}:</p>
-                        <p>
+                        <p className="flex items-center"
+                        >
                             <span className="text-lg mr-2">{baseInfo?.total_lines}</span>
-                            (添加
-                            <span className="text-green-600 inline-block mx-2">{baseInfo?.total_added}</span> 行,删除
-                            <span className="text-red-600 inline-block mx-2">{baseInfo?.total_deleted}</span>
-                            行)
+                            {i18n.language === 'zh' && <>
+                                (添加
+                                <span className="text-green-600 inline-block mx-2">{baseInfo?.total_added}</span> 行,删除
+                                <span className="text-red-600 inline-block mx-2">{baseInfo?.total_deleted}</span>
+                                行)
+                            </>}
+                            {i18n.language === 'en' && <>
+                                (
+                                <span className="text-green-600 inline-block mx-2">{baseInfo?.total_added}</span> added,
+                                <span className="text-red-600 inline-block mx-2">{baseInfo?.total_deleted}</span> removed
+                                )
+                            </>}
                         </p>
                     </div>
                     <div className="flex flex-row gap-10 text-right">
